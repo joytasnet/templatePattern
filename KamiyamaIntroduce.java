bfb1b74e7ddd827139d252db4c6f0e1553476811
kamiyama <chidayamika02@gmail.com>
2023-06-08 11:47:42 +0900

    KamiyamaIntroduce作成

---
+++ KamiyamaIntroduce.java
@@ -0,0 +1,10 @@
+public class KamiyamaIntroduce extends Introduce{
+	public KamiyamaIntroduce(String name){
+		super(name);
+	}
+	@Override
+	public void tokugi(){
+		System.out.println("ゲームへの理解が早いです");
+	}
+}
+

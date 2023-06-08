df8701c8d4f42f53b42b5d1f1e3284a8d2d06642
n.tsumugi <n.tsumugi@gmail.com>
2023-06-08 11:47:45 +0900

    NishinoharaIntrodece作成

---
+++ NishinoharaIntroduce.java
@@ -0,0 +1,10 @@
+public class NishinoharaIntroduce extends Introduce{
+	public NishinoharaIntroduce(String name){
+		super(name);
+	}
+
+	@Override
+	public void tokugi(){
+		System.out.println("絵を描くこと、口笛");
+	}
+}

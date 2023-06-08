9c7907e98af75f20bb6825b349c37f71ff4d7987
taku <rx77kz@docomo.ne.jp>
2023-06-08 11:47:40 +0900

    Introduce作成

---
+++ TakusariIntroduce.java
@@ -0,0 +1,9 @@
+public class TakusariIntroduce extends Introduce{
+	public TakusariIntroduce(String name){
+		super(name);
+	}
+  @Override
+  public void tokugi(){
+	  System.out.println("");
+	}
+}

d46f5bbe8a42c142fb3721ea2a716c9f077d8c51
io <kapi.name.pooh@gmail.com>
2023-06-08 11:47:35 +0900

    SasakiIntroduce作成

---
+++ SasakiIntroduce.java
@@ -0,0 +1,9 @@
+public class SasakiIntroduce extends Introduce{
+	public SasakiIntroduce(String name){
+		super(name);
+	}
+	@Override
+	public void tokugi(){
+		System.out.println("テニス,バスケ,blender");
+	}
+}

fb0ed2e328b89870df2a8187cced0281f184be7a
ruifukuda <rufk222@gmail.com>
2023-06-08 11:47:29 +0900

    FukudaIntroduce作成

---
+++ FukudaIntroduce.java
@@ -0,0 +1,9 @@
+public class FukudaIntroduce extends Introduce{
+	public FukudaIntroduce(String name){
+		super(name);
+	}
+	@Override
+	public void tokugi(){
+		System.out.println("逆立ちで階段登れます。");
+	}
+}
